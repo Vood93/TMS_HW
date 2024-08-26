8e756c2edb4b338c0aca59db3f494d3bf3bf1bfb
Vood93 <kolos1509@gmail.com>
2024-08-26 17:30:44 +0300

    add HW9 task8

---
+++ HW9/task8.py
@@ -0,0 +1,17 @@
+import json
+import csv
+
+
+def convert_json_to_csv(json_file, csv_file):
+    with open(json_file, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    with open(csv_file, "w", newline="", encoding="utf-8") as file:
+        csv_writer = csv.writer(file)
+
+        csv_writer.writerow(data[0].keys())
+        for row in data:
+            csv_writer.writerow(row.values())
+
+
+convert_json_to_csv("file.json", "convert_json.csv")
